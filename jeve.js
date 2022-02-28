79768fb33efd1b2a82e04ee3bfd1cceca084ba2e
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-02-28 20:38:22 +1000

    Add files via upload

--- jeve.js
+++ jeve.js
@@ -1,1 +1,16 @@
 https://teachablemachine.withgoogle.com/models/DZbUxZBd6/
+function Start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/DZbUxZBd6/model.json",modelLoaded);
+}
+function modelLoaded(){
+    classifier.classify(gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
