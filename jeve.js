2e7948a87c80a0a1c9404a49d061653b84fdf590
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-03-03 20:25:47 +1000

    Add files via upload

--- jeve.js
+++ jeve.js
@@ -1,5 +1,4 @@
-https://teachablemachine.withgoogle.com/models/DZbUxZBd6/
-function Start(){
+function startclassification(){
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/DZbUxZBd6/model.json",modelLoaded);
 }
@@ -12,5 +11,23 @@ function gotResult(error,results){
     }
     else{
         console.log(results);
+        r=Math.floor(Math.random()*255)-1;
+        g=Math.floor(Math.random()*255)-1;
+        b=Math.floor(Math.random()*255)-1;
+        document.getElementById("result_label").innerHTML="I can hear-"+results[0].label;
+        document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+        img=document.getElementById("animal");
+        if(results[0].label=="bark"){
+            img.src="dog.jpg";
+        }
+        if(results[0].label=="roar"){
+            img.src="lion.jpg";
+        }
+        if(results[0].label=="moo"){
+            img.src="cow.jpg";
+        }
+        if(results[0].label=="meow"){
+            img.src="cat.jpg";
+        }
     }
 }
